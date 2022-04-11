a91cbaba368a87f7f32b1ac43580f365a2cbe794
Etienne Roesch <etienne.roesch@gmail.com>
2022-04-11 10:01:02 -0700

    ENH: Added plugins/generic/openAIRE/

--- plugins/generic/openAIRE/OpenAIREPlugin.inc.php
+++ plugins/generic/openAIRE/OpenAIREPlugin.inc.php
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 2014-2019 Simon Fraser University
  * Copyright (c) 2003-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class OpenAIREPlugin
  * @ingroup plugins_generic_openAIRE
@@ -15,7 +15,6 @@
 
 import('lib.pkp.classes.plugins.GenericPlugin');
 
-
 class OpenAIREPlugin extends GenericPlugin {
 	/**
 	 * @copydoc Plugin::register()
@@ -23,49 +22,19 @@ class OpenAIREPlugin extends GenericPlugin {
 	function register($category, $path, $mainContextId = null) {
 		$success = parent::register($category, $path, $mainContextId);
 		if ($success && $this->getEnabled($mainContextId)) {
-			$this->import('OpenAIREDAO');
-			$openAIREDao = new OpenAIREDAO();
-			DAORegistry::registerDAO('OpenAIREDAO', $openAIREDao);
+			$this->import('OAIMetadataFormatPlugin_OpenAIRE');
+			PluginRegistry::register('oaiMetadataFormats', new OAIMetadataFormatPlugin_OpenAIRE($this), $this->getPluginPath());
+			$this->import('OpenAIREGatewayPlugin');
+			PluginRegistry::register('gateways', new OpenAIREGatewayPlugin($this), $this->getPluginPath());
 
-			// Insert new field into author metadata submission form (submission step 3) and metadata form
-			HookRegistry::register('Templates::Submission::SubmissionMetadataForm::AdditionalMetadata', array($this, 'metadataFieldEdit'));
+			# Handle COAR resource types in section forms
+			HookRegistry::register('sectiondao::getAdditionalFieldNames', array($this, 'addSectionDAOFieldNames'));			
+			HookRegistry::register('Templates::Manager::Sections::SectionForm::AdditionalMetadata', array($this, 'addSectionFormFields'));
+			HookRegistry::register('sectionform::initdata', array($this, 'initDataSectionFormFields'));
+			HookRegistry::register('sectionform::readuservars', array($this, 'readSectionFormFields'));
+			HookRegistry::register('sectionform::execute', array($this, 'executeSectionFormFields'));
 
-			// Hook for initData in two forms -- init the new field
-			HookRegistry::register('submissionsubmitstep3form::initdata', array($this, 'metadataInitData'));
-			HookRegistry::register('issueentrysubmissionreviewform::initdata', array($this, 'metadataInitData'));
-			HookRegistry::register('quicksubmitform::initdata', array($this, 'metadataInitData'));
-
-			// Hook for readUserVars in two forms -- consider the new field entry
-			HookRegistry::register('submissionsubmitstep3form::readuservars', array($this, 'metadataReadUserVars'));
-			HookRegistry::register('issueentrysubmissionreviewform::readuservars', array($this, 'metadataReadUserVars'));
-			HookRegistry::register('quicksubmitform::readuservars', array($this, 'metadataReadUserVars'));
-
-			// Hook for execute in two forms -- consider the new field in the article settings
-			HookRegistry::register('submissionsubmitstep3form::execute', array($this, 'metadataExecute'));
-			HookRegistry::register('issueentrysubmissionreviewform::execute', array($this, 'metadataExecute'));
-			HookRegistry::register('quicksubmitform::execute', array($this, 'metadataExecute'));
-
-			// Hook for save in two forms -- add validation for the new field
-			HookRegistry::register('submissionsubmitstep3form::Constructor', array($this, 'addCheck'));
-			HookRegistry::register('issueentrysubmissionreviewform::Constructor', array($this, 'addCheck'));
-			HookRegistry::register('quicksubmitform::Constructor', array($this, 'addCheck'));
-
-			// Consider the new field for ArticleDAO for storage
-			HookRegistry::register('articledao::getAdditionalFieldNames', array($this, 'articleSubmitGetFieldNames'));
-
-			// Add OpenAIRE set to OAI results
-			HookRegistry::register('OAIDAO::getJournalSets', array($this, 'sets'));
-			HookRegistry::register('JournalOAI::identifiers', array($this, 'recordsOrIdentifiers'));
-			HookRegistry::register('JournalOAI::records', array($this, 'recordsOrIdentifiers'));
-			HookRegistry::register('OAIDAO::_returnRecordFromRow', array($this, 'addSet'));
-			HookRegistry::register('OAIDAO::_returnIdentifierFromRow', array($this, 'addSet'));
-
-			 // Change Dc11Desctiption -- consider OpenAIRE elements relation, rights and date
-			HookRegistry::register('Dc11SchemaArticleAdapter::extractMetadataFromDataObject', array($this, 'changeDc11Desctiption'));
-
-			// consider OpenAIRE articles in article tombstones
-			HookRegistry::register('ArticleTombstoneManager::insertArticleTombstone', array($this, 'insertOpenAIREArticleTombstone'));
-
+			$this->_registerTemplateResource();
 		}
 		return $success;
 	}
@@ -84,247 +53,133 @@ class OpenAIREPlugin extends GenericPlugin {
 		return __('plugins.generic.openAIRE.description');
 	}
 
-
-	/*
-	 * Metadata
+	/**
+	 * Add section settings to SectionDAO
+	 *
+	 * @param $hookName string
+	 * @param $args array [
+	 *		@option SectionDAO
+	 *		@option array List of additional fields
+	 * ]
 	 */
+	public function addSectionDAOFieldNames($hookName, $args) {
+		$fields =& $args[1];
+		$fields[] = 'resourceType';
+	}
 
 	/**
-	 * Insert projectID field into author submission step 3 and metadata edit form
+	 * Add fields to the section editing form
+	 *
+	 * @param $hookName string `Templates::Manager::Sections::SectionForm::AdditionalMetadata`
+	 * @param $args array [
+	 *		@option array [
+	 *				@option name string Hook name
+	 *				@option sectionId int
+	 *		]
+	 *		@option Smarty
+	 *		@option string
+	 * ]
+	 * @return bool
 	 */
-	function metadataFieldEdit($hookName, $params) {
-		$smarty =& $params[1];
-		$output =& $params[2];
-
-		$output .= $smarty->fetch($this->getTemplateResource('projectIDEdit.tpl'));
+	public function addSectionFormFields($hookName, $args) {
+		$smarty =& $args[1];
+		$output =& $args[2];
+		$smarty->assign('resourceTypeOptions', $this->_getResourceTypeOptions());
+		$output .= $smarty->fetch($this->getTemplateResource('controllers/grids/settings/section/form/sectionFormAdditionalFields.tpl'));
 		return false;
 	}
 
 	/**
-	 * Add projectID element to the article
+	 * Initialize data when form is first loaded
+	 *
+	 * @param $hookName string `sectionform::initData`
+	 * @parram $args array [
+	 *		@option SectionForm
+	 * ]
 	 */
-	function articleSubmitGetFieldNames($hookName, $params) {
-		$fields =& $params[1];
-		$fields[] = 'projectID';
-		return false;
+	public function initDataSectionFormFields($hookName, $args) {
+		$sectionForm = $args[0];
+		$request = Application::getRequest();
+		$context = $request->getContext();
+		$contextId = $context ? $context->getId() : CONTEXT_ID_NONE;
+		$sectionDao = DAORegistry::getDAO('SectionDAO');
+		$section = $sectionDao->getById($sectionForm->getSectionId(), $contextId);
+		if ($section) $sectionForm->setData('resourceType', $section->getData('resourceType'));
 	}
 
 	/**
-	 * Set article projectID
+	 * Read user input from additional fields in the section editing form
+	 *
+	 * @param $hookName string `sectionform::readUserVars`
+	 * @parram $args array [
+	 *		@option SectionForm
+	 *		@option array User vars
+	 * ]
 	 */
-	function metadataExecute($hookName, $params) {
-		$form =& $params[0];
-		switch (get_class($form)) {
-			case 'SubmissionSubmitStep3Form':
-			case 'QuickSubmitForm':
-				$article = $form->submission;
-				break;
-			case 'IssueEntrySubmissionReviewForm':
-				$article = $form->getSubmission();
-				break;
-			default: throw new Exception('Unknown class form ' . get_class($form));
+	public function readSectionFormFields($hookName, $args) {
+		$sectionForm =& $args[0];
+		$request = Application::getRequest();
+		$sectionForm->setData('resourceType', $request->getUserVar('resourceType'));
+	}
+
+	/**
+	 * Save additional fields in the section editing form
+	 *
+	 * @param $hookName string `sectionform::execute`
+	 * @param $args array
+	 *
+	 */
+	public function executeSectionFormFields($hookName, $args) {
+		$sectionForm = $args[0];
+		$resourceType = $sectionForm->getData('resourceType') ? $sectionForm->getData('resourceType') : '';
+		if (!empty($resourceType)) {
+			$sectionDao = DAORegistry::getDAO('SectionDAO');
+			$section = $sectionDao->getById($sectionForm->getSectionId());			
+			$section->setData('resourceType', $resourceType);
+			$sectionDao->updateObject($section);
 		}
-		$formProjectID = $form->getData('projectID');
-		$article->setData('projectID', $formProjectID);
-		return false;
 	}
 
 	/**
-	 * Add check/validation for the projectID field (= 6 numbers)
+	 * Get a COAR Resource Type by URI. If $uri is null return all.
+	 * @param $uri string
+	 * @return mixed
 	 */
-	function addCheck($hookName, $params) {
-		$form =& $params[0];
-		if (get_class($form) == 'SubmissionSubmitStep3Form' ||
-			get_class($form) == 'IssueEntrySubmissionReviewForm' ||
-			get_class($form) == 'QuickSubmitForm' ) {
-			$form->addCheck(new FormValidatorRegExp($form, 'projectID', 'optional', 'plugins.generic.openAIRE.projectIDValid', '/^\d{6}$/'));
+	function _getCoarResourceType($uri = null) {
+		$resourceTypes = array(
+				'http://purl.org/coar/resource_type/c_6501' => 'journal article',
+				'http://purl.org/coar/resource_type/c_2df8fbb1' => 'research article',
+				'http://purl.org/coar/resource_type/c_dcae04bc' => 'review article',
+				'http://purl.org/coar/resource_type/c_beb9' => 'data paper',
+				'http://purl.org/coar/resource_type/c_7bab' => 'software paper',
+				'http://purl.org/coar/resource_type/c_b239' => 'editorial',
+				'http://purl.org/coar/resource_type/c_545b' => 'letter to the editor',
+				'http://purl.org/coar/resource_type/c_93fc' => 'report',
+				'http://purl.org/coar/resource_type/c_efa0' => 'review',
+				'http://purl.org/coar/resource_type/c_ba08' => 'book review',
+				'http://purl.org/coar/resource_type/c_5794' => 'conference paper',
+				'http://purl.org/coar/resource_type/c_46ec' => 'thesis',
+				'http://purl.org/coar/resource_type/c_8042' => 'working paper',
+				'http://purl.org/coar/resource_type/c_816b' => 'preprint'
+		);
+		if ($uri){
+			return $resourceTypes[$uri];
+		} else {
+			return $resourceTypes;
 		}
-		return false;
 	}
 
 	/**
-	 * Init article projectID
+	 * Get an associative array of all COAR Resource Type Genres for select element
+	 * (Includes default '' => "Choose One" string.)
+	 * @return array resourceTypeUri => resourceTypeLabel
 	 */
-	function metadataInitData($hookName, $params) {
-		$form =& $params[0];
-		if (get_class($form) == 'SubmissionSubmitStep3Form') {
-			$article = $form->submission;
-		} elseif (get_class($form) == 'IssueEntrySubmissionReviewForm') {
-			$article = $form->getSubmission();
-		} elseif (get_class($form) == 'QuickSubmitForm') {
-			$article = $form->submission;
-		}
-		$articleProjectID = $article->getData('projectID');
-		$form->setData('projectID', $articleProjectID);
-		return false;
+	function _getResourceTypeOptions() {		
+		$resourceTypeOptions = $this->_getCoarResourceType(null);
+		$chooseOne = __('common.chooseOne');
+		$chooseOneOption = array('' => $chooseOne);
+		$resourceTypeOptions  = $chooseOneOption + $resourceTypeOptions ;
+		return $resourceTypeOptions;
 	}
-
-	/**
-	 * Concern projectID field in the form
-	 */
-	function metadataReadUserVars($hookName, $params) {
-		$userVars =& $params[1];
-		$userVars[] = 'projectID';
-		return false;
-	}
-
-
-	/*
-	 * OAI interface
-	 */
-
-	/**
-	 * Add OpenAIRE set
-	 */
-	function sets($hookName, $params) {
-		$sets =& $params[5];
-		array_push($sets, new OAISet('ec_fundedresources', 'EC_fundedresources', ''));
-		return false;
-	}
-
-	/**
-	 * Get OpenAIRE records or identifiers
-	 */
-	function recordsOrIdentifiers($hookName, $params) {
-		$journalOAI =& $params[0];
-		$from = $params[1];
-		$until = $params[2];
-		$set = $params[3];
-		$offset = $params[4];
-		$limit = $params[5];
-		$total = $params[6];
-		$records =& $params[7];
-
-		$records = array();
-		if (isset($set) && $set == 'ec_fundedresources') {
-			$openAIREDao = DAORegistry::getDAO('OpenAIREDAO');
-			$openAIREDao->setOAI($journalOAI);
-			if ($hookName == 'JournalOAI::records') {
-				$funcName = '_returnRecordFromRow';
-			} else if ($hookName == 'JournalOAI::identifiers') {
-				$funcName = '_returnIdentifierFromRow';
-			}
-			$journalId = $journalOAI->journalId;
-			$records = $openAIREDao->getOpenAIRERecordsOrIdentifiers(array($journalId, null), $from, $until, $offset, $limit, $total, $funcName);
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Change OAI record or identifier to consider the OpenAIRE set
-	 */
-	function addSet($hookName, $params) {
-		$record =& $params[0];
-		$row = $params[1];
-
-		$openAIREDao = DAORegistry::getDAO('OpenAIREDAO');
-		if ($openAIREDao->isOpenAIRERecord($row)) {
-			$record->sets[] = 'ec_fundedresources';
-		}
-		return false;
-	}
-
- 	/**
-	 * Change Dc11 Description to consider the OpenAIRE elements
-	 */
-	function changeDc11Desctiption($hookName, $params) {
-		$adapter =& $params[0];
-		$article = $params[1];
-		$journal = $params[2];
-		$issue = $params[3];
-		$dc11Description =& $params[4];
-
-		$openAIREDao = DAORegistry::getDAO('OpenAIREDAO');
-		$openAIREDao->setOAI($journalOAI);
-		if ($openAIREDao->isOpenAIREArticle($article->getId())) {
-
-			// Determine OpenAIRE DC elements values
-			// OpenAIRE DC Relation
-			$articleProjectID = $article->getData('projectID');
-			$openAIRERelation = 'info:eu-repo/grantAgreement/EC/FP7/' . $articleProjectID;
-
-			// OpenAIRE DC Rights
-			$openAIRERights = 'info:eu-repo/semantics/';
-			$status = '';
-			if ($journal->getSetting('publishingMode') == PUBLISHING_MODE_OPEN) {
-				$status = 'openAccess';
-			} else if ($journal->getSetting('publishingMode') == PUBLISHING_MODE_SUBSCRIPTION) {
-				if ($issue->getAccessStatus() == 0 || $issue->getAccessStatus() == ISSUE_ACCESS_OPEN) {
-					$status = 'openAccess';
-				} else if ($issue->getAccessStatus() == ISSUE_ACCESS_SUBSCRIPTION) {
-					if (is_a($article, 'PublishedArticle') && $article->getAccessStatus() == ARTICLE_ACCESS_OPEN) {
-						$status = 'openAccess';
-					} else if ($issue->getAccessStatus() == ISSUE_ACCESS_SUBSCRIPTION && $issue->getOpenAccessDate() != NULL) {
-						$status = 'embargoedAccess';
-					} else if ($issue->getAccessStatus() == ISSUE_ACCESS_SUBSCRIPTION && $issue->getOpenAccessDate() == NULL) {
-						$status = 'closedAccess';
-					}
-				}
-			}
-			if ($journal->getSetting('restrictSiteAccess') == 1 || $journal->getSetting('restrictArticleAccess') == 1) {
-				$status = 'restrictedAccess';
-			}
-			$openAIRERights = $openAIRERights . $status;
-
-			// OpenAIRE DC Date
-			$openAIREDate = null;
-			if ($status == 'embargoedAccess') {
-				$openAIREDate = 'info:eu-repo/date/embargoEnd/' . date('Y-m-d', strtotime($issue->getOpenAccessDate()));
-			}
-
-			// Get current DC statements
-			$dcRelationValues = array();
-			$dcRightsValues = array();
-			$dcDateValues = array();
-			if ($dc11Description->hasStatement('dc:relation')) {
-				$dcRelationValues = $dc11Description->getStatement('dc:relation');
-			}
-			if ($dc11Description->hasStatement('dc:rights')) {
-				$dcRightsValues = $dc11Description->getStatementTranslations('dc:rights');
-			}
-			if ($dc11Description->hasStatement('dc:date')) {
-				$dcDateValues = $dc11Description->getStatement('dc:date');
-			}
-
-			// Set new DC statements, concerning OpenAIRE
-			array_unshift($dcRelationValues, $openAIRERelation);
-			$newDCRelationStatements = array('dc:relation' => $dcRelationValues);
-			$dc11Description->setStatements($newDCRelationStatements);
-
-			foreach ($dcRightsValues as $key => $value) {
-				array_unshift($value, $openAIRERights);
-				$dcRightsValues[$key] = $value;
-			}
-			if (!array_key_exists($journal->getPrimaryLocale(), $dcRightsValues)) {
-				$dcRightsValues[$journal->getPrimaryLocale()] = array($openAIRERights);
-			}
-			$newDCRightsStatements = array('dc:rights' => $dcRightsValues);
-			$dc11Description->setStatements($newDCRightsStatements);
-
-			if ($openAIREDate != null) {
-				array_unshift($dcDateValues, $openAIREDate);
-				$newDCDateStatements = array('dc:date' => $dcDateValues);
-				$dc11Description->setStatements($newDCDateStatements);
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * Consider the OpenAIRE set in the article tombstone
-	 */
-	function insertOpenAIREArticleTombstone($hookName, $params) {
-		$articleTombstone =& $params[0];
-
-		$openAIREDao = DAORegistry::getDAO('OpenAIREDAO');
-		if ($openAIREDao->isOpenAIREArticle($articleTombstone->getDataObjectId())) {
-			$dataObjectTombstoneSettingsDao = DAORegistry::getDAO('DataObjectTombstoneSettingsDAO');
-			$dataObjectTombstoneSettingsDao->updateSetting($articleTombstone->getId(), 'openaire', true, 'bool');
-		}
-		return false;
-	}
-
-
 }
 
