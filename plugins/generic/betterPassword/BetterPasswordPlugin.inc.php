b0ab6a47027a744e24e42dfcd65139662911bd4c
Etienne Roesch <etienne.roesch@gmail.com>
2022-04-11 10:02:47 -0700

    ENH: Added plugings/generic/betterPassword/

---
+++ plugins/generic/betterPassword/BetterPasswordPlugin.inc.php
@@ -0,0 +1,307 @@
+<?php
+
+/**
+ * @file plugins/generic/betterPassword/betterPasswordPlugin.inc.php
+ *
+ * Copyright (c) 2019 University of Pittsburgh
+ * Distributed under the GNU GPL v2 or later. For full terms see the file docs/COPYING.
+ *
+ * @class betterPasswordPlugin
+ * @ingroup plugins_generic_betterPassword
+ *
+ * @brief Better Password plugin class
+ */
+
+import('lib.pkp.classes.plugins.GenericPlugin');
+
+class betterPasswordPlugin extends GenericPlugin {
+
+	/**
+	 * @var $settings array()
+	 *  This array associates available settings with setting types
+	 *  Name prefixes of betterPassword, betterPasswordCheck, and betterPasswordLock are magical
+	 *    "betterPassword" will trigger the setting to be saved within the plugin
+	 *    "betterPasswordCheck" will be a group of checkbox options
+	 *    "betterPasswordLock" will be a group of textfields
+	 */
+	public $settingsKeys = array(
+		'betterPasswordCheckAlpha' => 'bool',
+		'betterPasswordCheckUppercase' => 'bool',
+		'betterPasswordCheckLowercase' => 'bool',
+		'betterPasswordCheckNumber' => 'bool',
+		'betterPasswordCheckSpecial' => 'bool',
+		'betterPasswordCheckBlacklist' => 'bool',
+		'betterPasswordLockTries' => 'int',
+		'betterPasswordLockExpires' => 'int',
+		'betterPasswordLockSeconds' => 'int',
+		'minPasswordLength' => 'int',
+	);
+	
+
+	/**
+	 * @copydoc Plugin::register()
+	 */
+	function register($category, $path, $mainContextId = null) {
+		$success = parent::register($category, $path, $mainContextId);
+		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
+		if ($success && $this->getEnabled()) {
+			// Attach hooks
+			foreach (array('registrationform::validate', 'changepasswordform::validate', 'loginchangepasswordform::validate') as $hook) {
+				HookRegistry::register($hook, array(&$this, 'checkPassword'));
+			}
+			if ($this->getSetting(CONTEXT_SITE, 'betterPasswordLockTries')) {
+				// Register callback to add text to registration page
+				HookRegistry::register('TemplateManager::display', array($this, 'handleTemplateDisplay'));
+				// Add a handler to hijack login requests
+				HookRegistry::register('LoadHandler', array($this, 'callbackLoadHandler'));
+			}
+			HookRegistry::register('userdao::getAdditionalFieldNames', array(&$this, 'addUserSettings'));
+		}
+		return $success;
+	}
+
+	/**
+	 * Site-wide plugins should override this function to return true.
+	 *
+	 * @return boolean
+	 */
+	function isSitePlugin() {
+		return true;
+	}
+
+	/**
+	 * Get the display name of this plugin.
+	 * @return String
+	 */
+	function getDisplayName() {
+		return __('plugins.generic.betterPassword.displayName');
+	}
+
+	/**
+	 * Get a description of the plugin.
+	 * @return String
+	 */
+	function getDescription() {
+		return __('plugins.generic.betterPassword.description');
+	}
+
+	/**
+	 * @copydoc Plugin::getActions()
+	 */
+	function getActions($request, $verb) {
+		$router = $request->getRouter();
+		import('lib.pkp.classes.linkAction.request.AjaxModal');
+		return array_merge(
+			$this->getEnabled()?array(
+				new LinkAction(
+					'settings',
+					new AjaxModal(
+						$router->url($request, null, null, 'manage', null, array('verb' => 'settings', 'plugin' => $this->getName(), 'category' => 'generic')),
+						$this->getDisplayName()
+					),
+					__('manager.plugins.settings'),
+					null
+				),
+			):array(),
+			parent::getActions($request, $verb)
+		);
+	}
+
+	/**
+	 * @copydoc Plugin::manage()
+	 */
+	function manage($args, $request) {
+		$verb = $request->getUserVar('verb');
+		switch ($verb) {
+			case 'settings':
+				$templateMgr = TemplateManager::getManager();
+				$templateMgr->register_function('plugin_url', array($this, 'smartyPluginUrl'));
+				$context = $request->getContext();
+
+				$this->import('BetterPasswordSettingsForm');
+				$form = new BetterPasswordSettingsForm($this, $context ? $context->getId() : CONTEXT_SITE);
+				if ($request->getUserVar('save')) {
+					$form->readInputData();
+					if ($form->validate()) {
+						$form->execute();
+						return new JSONMessage(true);
+					}
+				} else {
+					$form->initData();
+				}
+				return new JSONMessage(true, $form->fetch($request));
+		}
+		return parent::manage($args, $request);
+	}
+
+	/**
+	 * Hook callback: check a password against requirements
+	 * @see Form::validate()
+	 */
+	function checkPassword($hookName, $args) {
+		// Supported hooks must be enumerated here to identify the content to be checked and the form being used
+		$form = NULL;
+		$data = array();
+		switch ($hookName) {
+			case 'registrationform::validate':
+			case 'changepasswordform::validate':
+			case 'loginchangepasswordform::validate':
+				$form = $args[0];
+				$password = $form->getData('password');
+				$errorField = 'password';
+				break;
+			default:
+				return false;
+		}
+		if (!$password) {
+			// Let the form itself handle the core required function
+			return false;
+		}
+		if ($this->getSetting(CONTEXT_SITE, 'betterPasswordCheckBlacklist')) {
+			$badPassword = false;
+			$lowerPassword = strtolower($password);
+			foreach ($this->getBlacklists() as $filename) {
+				/* In case of out-of-memory error, break glass
+				$passwordfile = fopen($filename, "r");
+				while (!feof($passwordfile)) {
+					$disallowed = fgets($passwordfile);
+					if ($lowerPassword === $disallowed) {
+						$badPassword = true;
+						break;
+					}
+				}
+				fclose($passwordfile);
+				 */
+				$passwordfile = file_get_contents($filename);
+				if (strpos($passwordfile, $lowerPassword) !== false) {
+					$badPassword = true;
+					break;
+				}
+			}
+			if ($badPassword) {
+				$form->addError($errorField, __('plugins.generic.betterPassword.validation.betterPasswordCheckBlacklist'));
+			}
+		}
+		if ($this->getSetting(CONTEXT_SITE, 'betterPasswordCheckAlpha')) {
+			if (!PKPString::regexp_match('/[[:alpha:]]/', $password)) {
+				$form->addError($errorField, __('plugins.generic.betterPassword.validation.betterPasswordCheckAlpha'));
+			}
+		}
+		if ($this->getSetting(CONTEXT_SITE, 'betterPasswordCheckNumber')) {
+			if (!PKPString::regexp_match('/\d/', $password)) {
+				$form->addError($errorField, __('plugins.generic.betterPassword.validation.betterPasswordCheckNumber'));
+			}
+		}
+		if ($this->getSetting(CONTEXT_SITE, 'betterPasswordCheckUppercase')) {
+			if (!PKPString::regexp_match('/[[:upper:]]/', $password)) {
+				$form->addError($errorField, __('plugins.generic.betterPassword.validation.betterPasswordCheckUppercase'));
+			}
+		}
+		if ($this->getSetting(CONTEXT_SITE, 'betterPasswordCheckLowercase')) {
+			if (!PKPString::regexp_match('/[[:lower:]]/', $password)) {
+				$form->addError($errorField, __('plugins.generic.betterPassword.validation.betterPasswordCheckLowercase'));
+			}
+		}
+		if ($this->getSetting(CONTEXT_SITE, 'betterPasswordCheckSpecial')) {
+			if (!PKPString::regexp_match('/[^[:alnum:]]/', $password)) {
+				$form->addError($errorField, __('plugins.generic.betterPassword.validation.betterPasswordCheckSpecial'));
+			}
+		}
+	}
+
+	/**
+	 * Get the filename(s) of password blacklists.
+	 * @return array filename strings
+	 */
+	function getBlacklists() {
+		return array(
+			$this->getPluginPath() . DIRECTORY_SEPARATOR . 'badPasswords' . DIRECTORY_SEPARATOR . 'badPasswords.txt',
+		);
+	}
+
+	/*
+	 * Hook callback: check for bad password attempts
+	 * @see TemplateManager::display()
+	 */
+	function handleTemplateDisplay($hookName, $args) {
+		$templateMgr = $args[0];
+		$template = $args[1];
+		if ($template === 'frontend/pages/userLogin.tpl') {
+			if ($templateMgr->getTemplateVars('error') === 'user.login.loginError') {
+				$username = $templateMgr->getTemplateVars('username');
+				$userDao = DAORegistry::getDAO('UserDAO');
+				$user = $userDao->getByUsername($username);
+				if (isset($user)) {
+					$count = $user->getData($this->getName()."::badPasswordCount");
+					$time = $user->getData($this->getName()."::badPasswordTime");
+					// expire old bad password attempts
+					if (($count || $time) && $time < time() - $this->getSetting(CONTEXT_SITE, 'betterPasswordLockExpires')) {
+						$count = 0;
+					}
+					// update the count and time to represent this failed attempt
+					$count++;
+					$time = time();
+					$user->setData($this->getName()."::badPasswordCount", $count);
+					$user->setData($this->getName()."::badPasswordTime", $time);
+					$userDao->updateObject($user);
+					// warn the user if count has been exceeded
+					if ($count >= $this->getSetting(CONTEXT_SITE, 'betterPasswordLockTries')) {
+						$templateMgr->assign('error', 'plugins.generic.betterPassword.validation.betterPasswordLocked');
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @see PKPComponentRouter::route()
+	 */
+	public function callbackLoadHandler($hookName, $args) {
+		if ($args[0] === "login" && $args[1] === "signIn") {
+			// Hijack the user's signin attempt, if frequent bad passwords are being tried
+			if (isset($_POST['username'])) {
+				$userDao = DAORegistry::getDAO('UserDAO');
+				$user = $userDao->getByUsername($_POST['username']);
+				if (isset($user)) {
+					$count = $user->getData($this->getName()."::badPasswordCount");
+					$time = $user->getData($this->getName()."::badPasswordTime");
+					if ($count >= $this->getSetting(CONTEXT_SITE, 'betterPasswordLockTries') && $time > time() - $this->getSetting(CONTEXT_SITE, 'betterPasswordLockSeconds')) {
+						// Hijack the typical login/signIn handler to prevent login
+						define('HANDLER_CLASS', 'BetterPasswordHandler');
+						$args[0] = "plugins.generic.betterPassword.BetterPasswordHandler";
+						import($args[0]);
+						return true;
+					}
+				}
+			}
+		} elseif ($args[0] === "login" && $args[1] === "resetPassword") {
+			// Hijack the password reset request to clear bad password locks
+			$username = array_shift(PKPRequest::getRequestedArgs());
+			$queryString = PKPRequest::getQueryArray();
+			$userDao = DAORegistry::getDAO('UserDAO');
+			$confirmHash = $queryString['confirm'];
+			$user = $userDao->getByUsername($username);
+			if ($user && $confirmHash && Validation::verifyPasswordResetHash($user->getId(), $confirmHash)) {
+					$user->setData($this->getName()."::badPasswordCount", 0);
+					$user->setData($this->getName()."::badPasswordTime", 0);
+					$userDao->updateObject($user);
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Add locking information to the User DAO
+	 * @see DAO::getAddtionalFieldNames
+	 */
+	function addUserSettings($hookname, $args) {
+		$fields =& $args[1];
+		$fields = array_merge($fields, array(
+			$this->getName()."::badPasswordCount",
+			$this->getName()."::badPasswordTime",
+			)
+		);
+		return false;
+	}
+}
---
+++ plugins/generic/betterPassword/BetterPasswordSettingsForm.inc.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * @file plugins/generic/betterPassword/betterPasswordSettingsForm.inc.php
+ *
+ * Copyright (c) 2019 University of Pittsburgh
+ * Distributed under the GNU GPL v2 or later. For full terms see the file docs/COPYING.
+ *
+ * @class betterPasswordSettingsForm
+ * @ingroup plugins_generic_betterPassword
+ *
+ * @brief Form for administrators to modify Better Password plugin settings
+ */
+
+
+import('lib.pkp.classes.form.Form');
+
+class BetterPasswordSettingsForm extends Form {
+
+	/** @var $_contextId int */
+	var $_contextId;
+
+	/** @var $_plugin betterPasswordPlugin */
+	var $_plugin;
+
+	/** @var $_dependentFieldSemaphore bool flag if the dependent field error is already set */
+	var $_dependentFieldSemaphore = false;
+
+	/**
+	 * Constructor
+	 * @param $plugin BetterPasswordPlugin
+	 * @param $contextId int (not used)
+	 */
+	function __construct($plugin, $contextId) {
+		$this->_contextId = CONTEXT_SITE;
+		$this->_plugin = $plugin;
+		
+		parent::__construct($plugin->getTemplateResource('settingsForm.tpl'), $this->_contextId);
+
+		$lockFields = array();
+		foreach (array_keys($this->_plugin->settingsKeys) as $key) {
+			if (strpos($key, 'betterPasswordLock') === 0) {
+				$lockFields[] = $key;
+			}
+		}
+
+		foreach ($lockFields as $field) {
+			$this->addCheck(new FormValidatorCustom($this, $field, FORM_VALIDATOR_OPTIONAL_VALUE, 'plugins.generic.betterPassword.manager.settings.betterPasswordLockRequired', array(&$this, '_dependentFormFieldIsSet'), array(&$this, $lockFields)));
+			$this->addCheck(new FormValidatorCustom($this, $field, FORM_VALIDATOR_OPTIONAL_VALUE, 'plugins.generic.betterPassword.manager.settings.'.$field.'NumberRequired', create_function('$s', 'return ($s === "0" || $s > 0);')));
+		}
+
+		$this->addCheck(new FormValidatorPost($this));
+		$this->addCheck(new FormValidatorCSRF($this));
+	}
+
+	/**
+	 * Initialize form data.
+	 */
+	function initData() {
+		$contextId = $this->_contextId;
+		$plugin =& $this->_plugin;
+
+		parent::initData();
+		foreach (array_keys($plugin->settingsKeys) as $k) {
+			if (strpos($k, 'betterPassword') === 0) {
+				$this->setData($k, $plugin->getSetting($contextId, $k));
+			} else {
+				$siteDao = DAORegistry::getDAO('SiteDAO');
+				$site = $siteDao->getSite();
+				$this->setData($k, $site->getMinPasswordLength());
+			}
+		}
+	}
+
+	/**
+	 * Assign form data to user-submitted data.
+	 */
+	function readInputData() {
+		$this->readUserVars(array_keys($this->_plugin->settingsKeys));
+	}
+
+	/**
+	 * Fetch the form.
+	 * @copydoc Form::fetch()
+	 */
+	function fetch($request, $template = NULL, $display = false) {
+		AppLocale::requireComponents(LOCALE_COMPONENT_PKP_ADMIN);
+		$templateMgr = TemplateManager::getManager($request);
+		$templateMgr->assign('pluginName', $this->_plugin->getName());
+		foreach (array_keys($this->_plugin->settingsKeys) as $key) {
+			if (strpos($key, 'betterPasswordCheck') === 0) {
+				$checkboxes[$key] = $this->getData($key);
+			}
+			if (strpos($key, 'betterPasswordLock') === 0) {
+				$locking[$key] = $this->getData($key) ? $this->getData($key) : '';
+			}
+		}
+		$templateMgr->assign('betterPasswordCheckboxes', $checkboxes);
+		$templateMgr->assign('betterPasswordLocking', $locking);
+		return parent::fetch($request);
+	}
+
+	/**
+	 * Save settings.
+	 */
+	function execute() {
+		$plugin =& $this->_plugin;
+		$contextId = $this->_contextId;
+
+		foreach ($plugin->settingsKeys as $k => $v) {
+			$saveData = $this->getData($k);
+			$saveType = $v;
+			switch ($v) {
+				case 'bool':
+					$saveData = boolval($saveData);
+					break;
+				case 'int':
+					$saveData = intval($saveData);
+					break;
+			}
+			if (strpos($k, 'betterPassword') === 0) {
+				$plugin->updateSetting($contextId, $k, $saveData, $saveType);
+			} else {
+				$siteDao = DAORegistry::getDAO('SiteDAO');
+				$site = $siteDao->getSite();
+				$site->setMinPasswordLength(intval($saveData));
+				$siteDao->updateObject($site);
+			}
+		}
+	}
+
+	/**
+	 * Check for the presence of dependent fields if a field value is set
+	 * @param $fieldValue mixed the value of the field being checked
+	 * @param $form object a reference to this form
+	 * @param $dependentFields array a list of dependent field names
+	 * @return boolean
+	 */
+	function _dependentFormFieldIsSet($fieldValue, $form, $dependentFields) {
+		if ($fieldValue && !$this->_dependentFieldSemaphore) {
+			$dependentValues = true;
+			foreach ($dependentFields as $field) {
+				if (!$form->getData($field)) {
+					$dependentValues = false;
+				}
+			}
+			if ($dependentValues) {
+				// Field was set and dependent values are present
+				return true;
+			} else {
+				// Field was set but dependent value was missing
+				$this->_dependentFieldSemaphore = true;
+				return false;
+			}
+		} else {
+			// No value set, so no dependency
+			return true;
+		}
+		return true;
+	}
+}
